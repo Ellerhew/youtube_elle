7ecd12eedafa02b3988c183f1c09a32bc2e95dce
ellerhew <lovedory43@naver.com>
2021-04-28 22:10:26 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,26 @@
-
-import './app.css';
+import React, { useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return (
-    <h1>Hello :)</h1>
-  );
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		const requestOptions = {
+			method: "GET",
+			redirect: "follow",
+		};
+
+		fetch(
+			"https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyB_BjNheyaHqZfJ_o6Xzbec1CakM0UBE64",
+			requestOptions,
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((result) => setVideos(result.items))
+			.catch((error) => console.log("error", error));
+	}, []);
+	return <VideoList videos={videos} />;
 }
 
 export default App;
