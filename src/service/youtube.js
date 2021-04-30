6af4581c35f5c1ace78163fe644fba2cc31f32d6
ellerhew <lovedory43@naver.com>
2021-04-30 15:24:16 +0900

    add axios and responsive to screen size

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,32 +1,35 @@
+import axios from "axios";
+
 class Youtube {
 	constructor(key) {
-		this.key = key;
-		this.getRequestOptions = {
-			method: "GET",
-			redirect: "follow",
-		};
+		this.youtube = axios.create({
+			baseURL: `https://www.googleapis.com/youtube/v3`,
+			params: { key: key },
+		});
 	}
 
-	search(query) {
-		return fetch(
-			`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&key=${this.key}`,
-			this.getRequestOptions
-		)
-			.then((response) => response.json())
-			.then((result) =>
-				result.items.map((item) => ({ ...item, id: item.id.videoId }))
-			)
-			.catch((error) => console.log("error", error));
+	async search(query) {
+		const response = await this.youtube.get("search", {
+			params: {
+				part: "snippet",
+				maxResults: 25,
+				q: query,
+			},
+		});
+
+		return response.data.items;
 	}
 
-	mostPopular() {
-		return fetch(
-			`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-			this.getRequestOptions
-		)
-			.then((response) => response.json())
-			.then((result) => result.items)
-			.catch((error) => console.log("error", error));
+	async mostPopular() {
+		const response = await this.youtube.get("videos", {
+			params: {
+				part: "snippet",
+				chart: "mostPopular",
+				maxResults: 25,
+			},
+		});
+
+		return response.data.items;
 	}
 }
 
